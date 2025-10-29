bb78e7273eb4aed6eac3fbed071638bb92f10e84
pedr0almd <pdro.developer@gmail.com>
2025-10-29 20:47:30 -0300

    style(login): centraliza os elementos e ajusta as cores

--- front-end/src/pages/login.jsx
+++ front-end/src/pages/login.jsx
@@ -1,44 +1,55 @@
-import React from 'react'
-import { useNavigate } from 'react-router-dom';
-
-
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import logo from "../assets/images/logo-pina.png";
 
 function Login() {
-  
   //Função para navegar entre páginas do Login para Cadastro adicionando funcionalidade ao botão
   const navigate = useNavigate();
-    function irPara(rota) {
-    navigate(rota)
+  function irPara(rota) {
+    navigate(rota);
   }
 
   return (
-    <div>
+    <div className="login-container">
+      <div className="left-content">
+        <img src={logo} alt="Logo" />
+        <h1>Bem-vindo de volta!</h1>
 
-      <div classNam="left-content">
-        <img src="../public/react.svg" alt="Logo" />
-        <h1>BEM-VINDO DE VOLTA!</h1>
-        <form id="login-form">
-          <label htmlFor="email">Email</label>
-          <input type="email" placeholder='exmplo@gmail.com' required/>
+        <form id="login-form" className="input-group">
+          <div className="email-group">
+            <label htmlFor="email">Email</label>
+            <input type="email" placeholder="exemplo@email.com" required />
+          </div>
+          <div className="password-group">
+            <label htmlFor="password">Senha</label>
+            <input type="password" placeholder="Senha" required />
+            <div className="forgot-my-password-group">
+              <button
+                className="hiperlink btn-"
+                onClick={() => irPara("/redefinir-senha")}
+              >
+                Esqueci minha senha
+              </button>
+            </div>
+          </div>
 
-          <label htmlFor="password">Senha</label>
-          <input type="password" placeholder='Senha' required/>
-
-          <button onClick={() => irPara("/redefinir-senha")}>Esqueci minha senha</button>
-          <button type="submit">Entrar</button>
-          
+          <button className="btn-primary" type="submit">
+            Entrar
+          </button>
         </form>
 
-      <p>Não tem uma conta? <button onClick={() => irPara("/cadastro")}>Ir para cadastro</button></p>
-
+        <p>
+          Não tem uma conta?
+          <button className="hiperlink" onClick={() => irPara("/cadastro")}>
+            Crie uma
+          </button>
+        </p>
       </div>
 
       <div className="right-content">
-        <img src="../public/vite.svg" alt="Foto Pinacoteca" />
+        <img src="" alt="Foto Pinacoteca" />
       </div>
     </div>
-
-  )
+  );
 }
-export default Login
-
+export default Login;
